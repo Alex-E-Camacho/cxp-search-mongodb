1057d7b6c06fd4792d98ea351337792db107962c
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-14 15:23:23 -0700

    Add and require dotenv package

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+const index = require('./routes/index');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
 
 app.listen(8000, () => {
     console.log('App listening on port 8000...')
 })
+
+module.exports = app;
