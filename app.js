42ea4307671e4a701c955125505dbbc43b7ea7e7
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-14 13:41:30 -0700

    Change to es6 syntax

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(8000, function() {
+app.listen(8000, () => {
     console.log('App listening on port 8000...')
 })
