8488a3f4d455171ecf6208a1d0f945e0ef714510
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-14 15:39:01 -0700

    Successfully redirect from / to /search

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const index = require('./routes/index');
+const search = require('./routes/search');
 require('dotenv').config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
+app.use('/search', search);
 
 app.listen(8000, () => {
     console.log('App listening on port 8000...')
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('success!')
+    res.redirect('/search');
 })
 
 module.exports = router;
