046669d0f6511ce5dbbef8e4415a911a170868c2
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-15 10:34:14 -0700

    Simple models created, Azure MongoDB connection created

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const index = require('./routes/index');
 const search = require('./routes/search');
 const result = require('./routes/result');
 require('dotenv').config();
+const mongoose = require('mongoose');
+const mongoDB = process.env.DB_URL;
+
+//Set up mongoose connection
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
---
+++ models/result.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let ResultSchema = new Schema(
+    {
+        url: { type: String, required: true },
+        search: { type: Schema.ObjectId, ref: 'Search', required: true }
+    }
+);
+
+module.exports = mongoose.model();
