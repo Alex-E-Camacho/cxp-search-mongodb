069de55b1631febc4818d4ae366951885952250f
Alex-E-Camacho <alex.e.camacho@gmail.com>
2018-05-19 19:31:42 -0700

    Successfully iterate over results array and save each individually

--- controllers/resultController.js
+++ controllers/resultController.js
@@ -1,27 +1,15 @@
 const Result = require('../models/result');
 
 exports.result_create_post = (req, res) => {
-    let newResult = new Result({ url: req.body.result, search: req.searchData });
+  req.body.results.forEach((resultUrl) => {
+    let newResult = new Result({ url: resultUrl, search: req.searchData });
     newResult.save((err, savedResult) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(savedResult);
-        }
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(savedResult);
+      }
     })
-};
-
-// exports.result_create_post = (req, res) => {
-//   req.body.results.forEach((resultUrl) => {
-//     let newResult = new Result({ url: resultUrl, search: req.searchData });
-//
-//     newResult.save((err, savedResult) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(savedResult);
-//       }
-//     })
-//   })
-//   res.send('All saved');
-// }
+  })
+  res.send('All saved');
+}
