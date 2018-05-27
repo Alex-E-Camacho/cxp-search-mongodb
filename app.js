553059bce5c2fbd90d0150afac21b2d1f71626c0
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-27 08:35:38 -0700

    Cors middleware added and working

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
